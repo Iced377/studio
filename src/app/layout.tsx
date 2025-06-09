13ea97ac16bd51ee1e804097f88cc6415b922c21
Abed El Izmirli <izmerly@gmail.com>
2025-06-09 07:32:45 +0000

    when i install this app on a mobile phone, I get this icon. can you upda

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,6 +41,8 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#27AE60" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
